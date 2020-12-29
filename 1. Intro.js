14e392ba10ac0ec3882453b076925e7146d5818d
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-12-29 23:00:33 +0530

    Create 1. Intro.js

---
+++ 1. Intro.js
@@ -0,0 +1,13 @@
+1. What is Jasmine?
+It is an open-source testing framework for JS. It is a unit-testing framework.
+Jasmine does not depend on any other JavaScript framework.
+Jasmine does not require any DOM.
+
+
+2. How to setup Jasmine?
+
+* search for jasmine on github. (https://github.com/jasmine/jasmine) 
+* navigate to releases folder. (https://github.com/jasmine/jasmine/releases)
+* select latest version & download zip of that version
+* unzip that version & copy those files to your project.
+* we require all files which are inside lib folder. (boot.js, jasmine.js, jasmine-html.js, jasmine.css, specRunner.html)
