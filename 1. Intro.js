83976dbcc17d95f5a6e84a9f3a3de605e3f742a9
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-12-29 23:38:58 +0530

    Update 1. Intro.js

--- 1. Intro.js
+++ 1. Intro.js
@@ -11,3 +11,14 @@ Jasmine does not require any DOM.
 * select latest version & download zip of that version
 * unzip that version & copy those files to your project.
 * we require all files which are inside lib folder. (boot.js, jasmine.js, jasmine-html.js, jasmine.css, specRunner.html)
+
+
+jasmine.js - this is core file of jasmine which contains all framework of jasmine
+
+jasmine-html.js - this is reporter file, which shows all details of pass & fail test cases
+
+boot.js - this is core file which initializes jasmine & jasmine-html. it performs all necessary initialization before executing anything.
+
+jasmine.css - it is styling for jasmine UI.
+
+specRunner.html - html for specrunner page
